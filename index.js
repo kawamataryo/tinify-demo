8d6791036465e49de464be88c6543f240c04b09a
ryo <ba068082@gmail.com>
2019-04-01 07:21:20 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const tinify = require('tinify');
+tinify.key = 'RwskgYFv5mvmstHOpfs3ZOtPA9TmPCJO';
+
+const srcDir = './src/';
+const destDir = './dest/';
+const targetFileType = /.*\.(png|PNG|jpeg|jpg|JPG|JPEG)$/;
+
+const optimizeImage = async function (file) {
+  const srcFile = srcDir + file;
+  const destFile = destDir + file;
+
+  // 最適化処理
+  await tinify.fromFile(srcFile).toFile(destFile);
+
+  // log
+  console.log(`${file}  => ${Math.round(
+      fs.statSync(destFile).size / fs.statSync(srcFile).size * 100)} %`);
+};
+
+fs.readdir(srcDir, (err, files) => {
+  // 対象のファイルに対して最適化を実行
+  files.filter(file => file.match(targetFileType)).forEach(file => {
+    optimizeImage(file)
+  })
+});
+
